89ced8b0dde5d6158be796fd02f7171c5e59387c
alireza <alireza.jvh98@gmail.com>
2025-04-25 18:25:08 +0200

    feat(api): implement basic of upload api

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI
+from api.v1 import router as v1_router
 
 app = FastAPI()
-
+app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
