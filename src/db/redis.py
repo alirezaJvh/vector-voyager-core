c6314c99707f0f3ed6a52ab792c38dc97de14021
alireza <alireza.jvh98@gmail.com>
2025-04-26 12:03:23 +0200

    refactor: create redis-client class

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,24 +1,6 @@
 import redis.asyncio as redis
 from .config import REDIS_PORT
 
-# redis_client: redis.Redis = None
-
-
-# async def get_redis() -> redis.Redis:
-    
-#     if not redis_client:
-#         try:
-#             redis_instance = await redis.from_url(
-#                 f"redis://redis:{REDIS_PORT}",
-#                 decode_responses=True,
-#             )
-#         except redis.Redis.RedisError as e:
-#             # TODO: app logger 
-#             # app_logger.error(f"Error connecting to Redis: {e}")
-#             print('### error')
-#             raise e
-#     return redis_instance
-
 class RedisClient:
     def __init__(self):
         self._client = None
