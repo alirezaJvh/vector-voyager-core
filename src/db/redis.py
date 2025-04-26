2fa033989bcb13a2a6b9e876d75fdc8aa140601f
alireza <alireza.jvh98@gmail.com>
2025-04-26 12:02:22 +0200

    src/db/redis.py

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,21 +1,47 @@
 import redis.asyncio as redis
 from .config import REDIS_PORT
 
-redis_client: redis.Redis = None
+# redis_client: redis.Redis = None
 
 
-async def get_redis() -> redis.Redis:
-    global redis_client
+# async def get_redis() -> redis.Redis:
     
-    if not redis_client:
-        try:
-            redis_instance = await redis.from_url(
-                f"redis://redis:{REDIS_PORT}",
-                decode_responses=True,
-            )
-        except redis.Redis.RedisError as e:
-            # TODO: app logger 
-            # app_logger.error(f"Error connecting to Redis: {e}")
-            print('### error')
-            raise e
-    return redis_instance
+#     if not redis_client:
+#         try:
+#             redis_instance = await redis.from_url(
+#                 f"redis://redis:{REDIS_PORT}",
+#                 decode_responses=True,
+#             )
+#         except redis.Redis.RedisError as e:
+#             # TODO: app logger 
+#             # app_logger.error(f"Error connecting to Redis: {e}")
+#             print('### error')
+#             raise e
+#     return redis_instance
+
+class RedisClient:
+    def __init__(self):
+        self._client = None
+
+    async def init_client(self):
+        if not self._client:
+            try:
+                self._client = await redis.from_url(
+                    f"redis://redis:{REDIS_PORT}",
+                    decode_responses=True,
+                )
+            except redis.Redis.RedisError as e:
+                # TODO: app logger 
+                # app_logger.error(f"Error connecting to Redis: {e}")
+                print('### error')
+                raise e
+
+    async def get_client(self) -> redis.Redis:
+        if not self._client:
+            await self.init_client()
+        return self._client
+    
+        
+
+
+    
--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from api.v1 import router as v1_router
 from db.vector_db import create_vector_db
-from db.redis import get_redis
+from db.redis import RedisClient
 
 vector_db = create_vector_db()
-# redis_client = get_redis()
-# redis_client = RedisClient()
+redis = RedisClient()
+
 app = FastAPI()
 app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/")
 async def read_root():
-    redis_client = await get_redis()
+    redis_client = await redis.get_client()
     a = await redis_client.get("test")
     return {"Hello": "World", "b": a}
 
