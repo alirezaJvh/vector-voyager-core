6916feba80a7b61b128cef50f33ca3c634bd34e9
alireza <alireza.jvh98@gmail.com>
2025-04-26 13:21:57 +0200

    feat(validation): add more csv validation

--- src/api/v1/routing.py
+++ src/api/v1/routing.py
@@ -7,18 +7,29 @@ router = APIRouter()
 
 # upload csv file to save it in vector database
 @router.post("/upload")
-async def upload_csv(file: UploadFile = File(...), required_header: str = None):
-    # Validate the CSV file using our Pydantic validator
+async def upload_csv(file: UploadFile = File(...), review_header: str = None, product_id_header: str = None):
+    if not review_header:
+        raise HTTPException(status_code=400, detail="review_header is required")
+    
+    if not product_id_header:
+        raise HTTPException(status_code=400, detail="product_id_header is required")
+    # Validate the CSV file
     data, file_size = await CSVFileValidator.validate_csv_file(
         file=file,
-        required_header=required_header,
+        required_header=[review_header, product_id_header]
     )
-    print('@@@@@@@@ data @@@@@@@@@')
-    print(len(data))
-    print('@@@@@@@@ file_size @@@@@@@@@')
-    print(file_size)
-    # print('@@@@@@@@ text @@@@@@@@@')
-    # text = next(data)
+    
+    print('@@@@@ data @@@@')
+    print(data)
+
+    # Extract column data
+    # data = [row[review_header] for row in data_dict 
+    #         if review_header in row and row[review_header]]
+
+    # print('@@@@@@@@ data @@@@@@@@@')
+    # print(len(csv_reader))
+    # print('@@@@@@@@ file_size @@@@@@@@@')
+    # print(file_size)
 
     # print(text)
     return {"status": "success"}
--- src/common/validation.py
+++ src/common/validation.py
@@ -5,27 +5,29 @@ from fastapi import UploadFile, HTTPException
 class CSVFileValidator:
     """Helper class for validating CSV files"""
     @staticmethod
-    async def validate_csv_file(file: UploadFile, required_header: str):
+    async def validate_csv_file(file: UploadFile, required_header: list[str] = []):
  
         if not file.filename.endswith('.csv'):
             raise HTTPException(status_code=400, detail="Invalid file type. Only CSV files are allowed.")
     
         contents = await file.read()
         file_size = len(contents)
-        data = []
         
         try:
             text_content = contents.decode('utf-8')
             csv_file = StringIO(text_content)
             csv_to_dict = csv.DictReader(csv_file)
-
-            for row in csv_to_dict:
-                print('@@@@@@ row @@@@@@@')
-                print(row[required_header])
-                if row[required_header]:
-                    data.append(row[required_header])
-            
+            data = list(csv_to_dict)
             await file.seek(0)
+
+            if not data:
+                raise HTTPException(status_code=400, detail="CSV file is empty")
+
+            if len(required_header):
+                for header in required_header:
+                    if header not in data[0]:
+                        raise HTTPException(status_code=400, detail=f"Header {header} is not found in the CSV file.")
+
             return data, file_size
             
         except UnicodeDecodeError:
