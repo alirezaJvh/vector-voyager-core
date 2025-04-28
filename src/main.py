793f09008db4063e4967af5a6dde839727a7695f
alireza <alireza.jvh98@gmail.com>
2025-04-28 16:54:29 +0200

    chore: add cors origin middaleware

--- src/main.py
+++ src/main.py
@@ -1,12 +1,36 @@
 from fastapi import FastAPI
 from api.v1 import router as v1_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
 app.include_router(v1_router, prefix="/api/v1")
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"],
+    allow_origins=["*"],
+    allow_headers=[
+        "accept",
+        "accept-encoding",
+        "authorization",
+        "content-type",
+        "dnt",
+        "origin",
+        "user-agent",
+        "x-csrftoken",
+        "x-requested-with",
+        "x-spoki-account",
+        "x-spoki-user",
+        "x-spoki-api-key",
+        "x-spoki-platform-version",
+        "x-secret",
+    ],
+)
+
 
 
 @app.get("/healthz")
